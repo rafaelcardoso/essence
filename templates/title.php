913d58725bd7caf85d2e7b1aa455c2b7693404fa
Kevin Martensson <kevinmartensson@gmail.com>
2013-01-13 06:08:25 +0100

    Abstract page title into a function

--- templates/title.php
+++ templates/title.php
@@ -8,45 +8,5 @@
 ?>
 
 <header>
-  <h1>
-    <?php
-      if (is_home()) {
-        if (get_option('page_for_posts', true)) {
-          echo get_the_title(get_option('page_for_posts', true));
-        } else {
-          _e('Latest posts', THEME_NAME);
-        }
-      }
-      elseif (is_archive()) {
-        $term = get_term_by('slug', get_query_var('term'), get_query_var('taxonomy'));
-        if ($term) {
-          echo $term->name;
-        } elseif (is_post_type_archive()) {
-          echo get_queried_object()->labels->name;
-        } elseif (is_day()) {
-          printf(__('Daily Archives: %s', THEME_NAME), get_the_date());
-        } elseif (is_month()) {
-          printf(__('Monthly Archives: %s', THEME_NAME), get_the_date('F Y'));
-        } elseif (is_year()) {
-          printf(__('Yearly Archives: %s', THEME_NAME), get_the_date('Y'));
-        } elseif (is_author()) {
-          global $post;
-          $author_id = $post->post_author;
-          printf(__('Author Archives: %s', THEME_NAME), get_the_author_meta('display_name', $author_id));
-        }
-        else {
-          single_cat_title();
-        }
-      }
-      elseif (is_search()) {
-        printf(__('Search Results for %s', THEME_NAME), get_search_query());
-      }
-      elseif (is_404()) {
-        _e('404 — page not found', THEME_NAME);
-      }
-      else {
-        the_title();
-      }
-    ?>
-  </h1>
+  <h1><?php echo essence_page_title(); ?></h1>
 </header>
